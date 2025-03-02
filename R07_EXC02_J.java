9797f94241be52e40a0bdf6a3438158467c991f2
laurahp00 <32912181+laurahp00@users.noreply.github.com>
2025-03-02 08:07:35 -0500

    Update R07_EXC02_J.java

--- R07_EXC02_J.java
+++ R07_EXC02_J.java
@@ -2,10 +2,26 @@
 // https://wiki.sei.cmu.edu/confluence/display/java/ERR01-J.+Do+not+allow+exceptions+to+expose+sensitive+information
 
 class ExceptionExample {
-  public static void main(String[] args) throws FileNotFoundException {
-    // Linux stores a user's home directory path in
-    // the environment variable $HOME, Windows in %APPDATA%
-    FileInputStream fis =
-        new FileInputStream(System.getenv("APPDATA") + args[0]); 
+  public static void main(String[] args) {
+ 
+    File file = null;
+    try {
+      file = new File(System.getenv("APPDATA") +
+             args[0]).getCanonicalFile();
+      if (!file.getPath().startsWith("c:\\homepath")) {
+        System.out.println("Invalid file");
+        return;
+      }
+    } catch (IOException x) {
+      System.out.println("Invalid file");
+      return;
+    }
+ 
+    try {
+      FileInputStream fis = new FileInputStream(file);
+    } catch (FileNotFoundException x) {
+      System.out.println("Invalid file");
+      return;
+    }
   }
 }
