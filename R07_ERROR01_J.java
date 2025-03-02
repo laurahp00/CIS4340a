183b526db04d2d27e79737c3a5544ada3736930c
laurahp00 <32912181+laurahp00@users.noreply.github.com>
2025-03-02 08:25:42 -0500

    Update R07_ERROR01_J.java

--- R07_ERROR01_J.java
+++ R07_ERROR01_J.java
@@ -1,6 +1,7 @@
 // compliant code, user example
 // https://wiki.sei.cmu.edu/confluence/display/java/ERR01-J.+Do+not+allow+exceptions+to+expose+sensitive+information
 
+// RULE ERROR01-J do not allow exceptions to expose sensitive information
 class ExceptionExample {
   public static void main(String[] args) {
  
