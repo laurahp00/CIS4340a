82d74d0ccbff273dc414529e494046846007d0fa
laurahp00 <32912181+laurahp00@users.noreply.github.com>
2025-03-02 08:21:03 -0500

    Update R03_NUM03_J.java

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,4 +1,5 @@
 // compliant code
+// RULE NUM03-J use integer types that can fully represent the possible range of unsigned data
 public static long getInteger(DataInputStream is) throws IOException {
   return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
 }
