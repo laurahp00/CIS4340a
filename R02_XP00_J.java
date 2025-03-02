49f1b5d3e718d43e04ab1eea24168516969fac1b
laurahp00 <32912181+laurahp00@users.noreply.github.com>
2025-03-02 07:55:45 -0500

    Update R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,8 +1,10 @@
-// noncompliant code
+// compliant code
 public void deleteFile(){
  
   File someFile = new File("someFileName.txt");
   // Do something with someFile
-  someFile.delete();
+  if (!someFile.delete()) {
+    // Handle failure to delete the file
+  }
  
 }
