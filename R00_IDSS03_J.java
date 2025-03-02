eeac2a8d5903cebba7424ff1a8bf9e0721f27208
laurahp00 <32912181+laurahp00@users.noreply.github.com>
2025-03-02 07:53:36 -0500

    Update R00_IDSS03_J.java
    fixed code commits

--- R00_IDSS03_J.java
+++ R00_IDSS03_J.java
@@ -1,6 +1,10 @@
-// noncompliant code
+// compliant code
 if (loginSuccessful) {
-    logger.severe("User login succeeded for: " + username);
-  } else {
-    logger.severe("User login failed for: " + username);
-  }
+  logger.severe("User login succeeded for: " + sanitizeUser(username));
+} else {
+  logger.severe("User login failed for: " + sanitizeUser(username));
+}
+public String sanitizeUser(String username) {
+  return Pattern.matches("[A-Za-z0-9_]+", username))
+      ? username : "unauthorized user";
+}
