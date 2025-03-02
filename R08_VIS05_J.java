b8fe5edefdc332e77c1e64f41b3cde8ec2d9ef16
laurahp00 <32912181+laurahp00@users.noreply.github.com>
2025-03-02 08:08:43 -0500

    Create R08_VIS05_J.java

---
+++ R08_VIS05_J.java
@@ -0,0 +1,13 @@
+// noncompliant code user example
+// https://wiki.sei.cmu.edu/confluence/display/java/VNA05-J.+Ensure+atomicity+when+reading+and+writing+64-bit+values
+class LongContainer {
+  private long i = 0;
+ 
+  void assignValue(long j) {
+    i = j;
+  }
+ 
+  void printLong() {
+    System.out.println("i = " + i);
+  }
+}
