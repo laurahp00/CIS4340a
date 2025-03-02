36e7ba65be2aa4319401ec9e19e60792f0561336
laurahp00 <32912181+laurahp00@users.noreply.github.com>
2025-03-02 08:26:04 -0500

    Update R08_VNA05_J.java

--- R08_VNA05_J.java
+++ R08_VNA05_J.java
@@ -1,6 +1,6 @@
 // compliant code user example
 // https://wiki.sei.cmu.edu/confluence/display/java/VNA05-J.+Ensure+atomicity+when+reading+and+writing+64-bit+values
-
+// RULE VNA05-J. Ensure atomicity when reading and writing 64-bit values
 class LongContainer {
   private volatile long i = 0;
  
